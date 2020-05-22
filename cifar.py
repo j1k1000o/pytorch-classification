a984eabf12d89a145b76807d9cd9ca62f2f56f2d
juancprzs <jc.perez13@uniandes.edu.co>
2020-05-22 12:02:17 -0500

    include image type for regular forward

--- cifar.py
+++ cifar.py
@@ -242,7 +242,7 @@ def train(trainloader, model, criterion, optimizer, epoch, lower_limit,
         data_time.update(time.time() - end)
         inputs, targets = inputs.to(device), targets.to(device)
         # compute output
-        outputs = model(inputs)
+        outputs = model(inputs, im_type='nat')
         loss = criterion(outputs, targets)
         # compute adv examples
         if args.advprop_lambda > 0.0:
@@ -306,7 +306,7 @@ def test(testloader, model, criterion, epoch):
         data_time.update(time.time() - end)
         inputs, targets = inputs.to(device), targets.to(device)
         # compute output
-        outputs = model(inputs)
+        outputs = model(inputs, im_type='nat')
         loss = criterion(outputs, targets)
 
         # measure accuracy and record loss
